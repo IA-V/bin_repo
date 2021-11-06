4a32aee93bd6079fe9a21150c02cc13013178e6b
IA-V <iagoviper48@gmail.com>
2021-11-06 18:36:45 -0300

    Att melhoria no script de upload

--- subir_alteracoes_git.sh
+++ subir_alteracoes_git.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git add .
-([ $# -eq 2 ] && git commit -m "$1" && git push origin $2) || echo "Missing argument(s)!"
-exit 0
+([ $# -eq 2 ] && git commit -m "$1" && git push origin $2 && exit 0) || echo "Missing argument(s)!" ; exit 1
