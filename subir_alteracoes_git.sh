9c1b17a3b66089bfb790c70263c360b1e5668485
IA-V <iagoviper48@gmail.com>
2021-11-08 17:40:06 -0300

    Att script commit\push

--- subir_alteracoes_git.sh
+++ subir_alteracoes_git.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-[ $# -eq 2 ] || echo "Missing argument(s)!"  || exit 1
-git add .
-git commit -m "$1" && git push origin $2
+if [ $# -ne 2 ] ; then
+    echo "Missing argument(s)!"
+    exit 1
+else
+    git add .
+    git commit -m "$1"
+    if [ $? -eq 0 ] || [ $? -eq 1 ] ; then
+        git push origin $2
+    fi
+fi
 exit 0
