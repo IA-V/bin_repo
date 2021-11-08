89f8c84f19f03e93e646a0a65826c53c9205793d
IA-V <iagoviper48@gmail.com>
2021-11-08 11:16:40 -0300

    Correcao script push

--- subir_alteracoes_git.sh
+++ subir_alteracoes_git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ $# -eq 2 ] || echo "Missing argument(s)!"  || exit 1
 git add .
-[ $# -eq 2 ] || (echo "Missing argument(s)!" ; exit 1)
-git commit -m "$1" && git push origin $2 && exit 0
+git commit -m "$1" && git push origin $2
+exit 0
